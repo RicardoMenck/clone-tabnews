59b6025680186f1165fa945914eb5d52e1cd3987
Ricardo Augusto <56685933+RicardoMenck@users.noreply.github.com>
2025-11-09 13:06:21 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+//file-Based Rounting baseado no php
+//Qaulquer arquivo que possuir um index vira uma rota pública, incluindo o index
+// site.com/
+
+function Home() {
+    return <h1>Página principal</h1>
+}
+
+export default Home;
