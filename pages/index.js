7641b755fd3df6c2c00c20f8ea1045c08fdad8a8
Ricardo Augusto <56685933+RicardoMenck@users.noreply.github.com>
2025-11-09 14:43:55 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-//file-Based Rounting baseado no php
-//Qaulquer arquivo que possuir um index vira uma rota pública, incluindo o index
-// site.com/
-
 function Home() {
-    return <h1>Página principal</h1>
+    return <h1>Página principal alterado</h1>
 }
 
 export default Home;
